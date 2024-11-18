4eec005fc462f85efa9c5ddbea3f1a0b8a4d20ac
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-18 08:30:44 -0500

    Update NixOS configuration

--- modules/scripts/flake-build.sh
+++ modules/scripts/flake-build.sh
@@ -12,12 +12,12 @@ echo "Checking for changes..."
 if ! git diff-index --quiet HEAD --; then
     echo "Changes detected. Committing to git..."
     git add .
-    read -p "Enter a commit message (default: 'Update NixOS configuration'): " commit_message
+    read -r -p "Enter a commit message (default: 'Update NixOS configuration'): " commit_message
     commit_message=${commit_message:-"Update NixOS configuration"}
     git commit -m "$commit_message"
 
     echo "Pushing changes to remote repository..."
-    read -p "Are you sure you want to force push to master? (y/N): " confirm
+    read -r -p "Are you sure you want to force push to master? (y/N): " confirm
     if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
         git push origin master --force
     else
@@ -29,6 +29,6 @@ fi
 
 # Rebuild NixOS
 echo "Rebuilding NixOS configuration..."
-sudo nixos-rebuild switch --flake $HOME/nixos-config --impure
+sudo nixos-rebuild switch --flake "$HOME"/nixos-config --impure
 
 echo "NixOS build and update complete!"
