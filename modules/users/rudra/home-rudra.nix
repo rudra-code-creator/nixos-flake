66e773aef6e0ae05f4d061922aad766f3046e637
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-19 02:51:30 -0500

    fixed bat

--- modules/users/rudra/home-rudra.nix
+++ modules/users/rudra/home-rudra.nix
@@ -42,17 +42,6 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = {
-        dracula = {
-          src = pkgs.fetchFromGitHub {
-            owner = "dracula";
-            repo = "sublime"; # Bat uses sublime syntax for its themes
-            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
-          };
-          file = "Dracula.tmTheme";
-        };
-      };
       italic-text = "always";
     };    
   };
