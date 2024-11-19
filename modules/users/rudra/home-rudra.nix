85d22d36e3d65bfafbeb5908a330b981e43d071a
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-19 02:50:24 -0500

    Update NixOS configuration

--- modules/users/rudra/home-rudra.nix
+++ modules/users/rudra/home-rudra.nix
@@ -42,7 +42,17 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "Github";
+      theme = {
+        dracula = {
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "sublime"; # Bat uses sublime syntax for its themes
+            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          };
+          file = "Dracula.tmTheme";
+        };
+      };
       italic-text = "always";
     };    
   };
