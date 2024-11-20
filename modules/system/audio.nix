a004d049577d2e873cd1de82f1642e2b627edd7d
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-20 07:22:11 -0500

    Update NixOS configuration

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -2,6 +2,7 @@
 
 {
   # Enable sound with pipewire
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -10,10 +11,9 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    # jack.enable = true;
-
+    jack.enable = true;
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    media-session.enable = true;
   };
 }
