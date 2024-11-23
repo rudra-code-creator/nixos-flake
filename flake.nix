ba31df076ff2e375b932672166bfd6d13cdcfd9c
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-23 00:35:33 -0500

    Update NixOS configuration

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, inputs, ... }: {
+  outputs = { self, nixpkgs, home-manager, inputs, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
