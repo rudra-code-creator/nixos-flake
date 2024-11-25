e3f3ae28345b3608c9bfa9d000a8c7be69472925
rudra-code-creator <rudrakeshwani2@gmail.com>
2024-11-25 04:30:45 -0500

    Update NixOS configuration

--- modules/users/rudra/home-rudra.nix
+++ modules/users/rudra/home-rudra.nix
@@ -207,6 +207,34 @@
     generateCaches = true;
   };
 
+  programs.nnn = {
+    enable = true;
+
+    package = pkgs.nnn.override { withNerdIcons = true; };
+    extraPackages = with pkgs; [ ffmpegthumbnailer mediainfo sxiv ];
+
+    bookmarks = {
+      d = "~/Documents";
+      D = "~/Downloads";
+      p = "~/Pictures";
+      v = "~/Videos";
+    };
+
+    plugins.mappings = {
+      c = "fzcd";
+      f = "finder";
+      v = "imgview";
+    };
+
+    plugins.src = (pkgs.fetchFromGitHub {
+      owner = "jarun";
+      repo = "nnn";
+      rev = "v4.0";
+      sha256 = "sha256-Hpc8YaJeAzJoEi7aJ6DntH2VLkoR6ToP6tPYn3llR7k=";
+    }) + "/plugins";
+
+  };
+
   programs.git = {
     enable = true;
     userName = "rudra-code-creator";
